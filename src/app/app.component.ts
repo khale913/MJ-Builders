9b4bb2b306957ff5671672395bca51d701a13214
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-16 13:31:37 -0700

    descriptions of services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jdhconstruction';
+
+  onActivate(event: any) {
+    // window.scroll(0,0);
+
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+
+  }
+  //   onActivate(event: any) {
+  //     let scrollToTop = window.setInterval(() => {
+  //         let pos = window.pageYOffset;
+  //         if (pos > 0) {
+  //             window.scrollTo(0, pos - 20); // how far to scroll on each step
+  //         } else {
+  //             window.clearInterval(scrollToTop);
+  //         }
+  //     }, 16);
+  // }
 }
