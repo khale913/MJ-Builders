c50dcc6a3ab7f22918ef74b37b7fef3580042b8b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-11-15 14:27:59 -0800

    our team setup

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -8,6 +8,26 @@ import { TabServiceService } from '../tab-service.service';
 })
 export class AboutUsComponent implements OnInit {
   count: number | undefined;
+  employees: any = [
+    {
+      name: 'Jarrett Hale',
+      title: 'CEO',
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus, esse soluta itaque ipsa, commo
+      unde, eligendi minus aliquam dolorum praesentium mollitia! Praesentium ratione ducimus iste possimus autem,
+      totam molestiae.lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quae corrupti quas tenetur
+      eaque ipsam. Veritatis laudantium voluptates, impedit provident est, harum minima quos assumenda quisquam a
+      illum placeat officia.`
+    },
+    {
+      name: 'Mark Mark',
+      title: 'CEO',
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus, esse soluta itaque ipsa, commo
+      unde, eligendi minus aliquam dolorum praesentium mollitia! Praesentium ratione ducimus iste possimus autem,
+      totam molestiae.lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quae corrupti quas tenetur
+      eaque ipsam. Veritatis laudantium voluptates, impedit provident est, harum minima quos assumenda quisquam a
+      illum placeat officia.`
+    }
+  ]
   constructor(private service: TabServiceService) { }
 
   ngOnInit(): void {
